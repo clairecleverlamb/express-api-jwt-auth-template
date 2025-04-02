30df726f8b1c2851eca1018b3c558005cc9674d2
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-02 10:14:31 -0700

    added poiler plate

---
+++ controllers/test-jwt.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/sign-token', (req, res) => {
+    res.json({ message: "You are authorized!" });
+});
+
+module.exports = router;
